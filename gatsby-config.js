dd8a4e4743741c5daa288ef98342351ccf0c431c
Jamie Liu <jamie.liu386@gmail.com>
2020-06-25 18:33:28 -0400

    Add eslint, husky, editorconfig

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,35 +1,37 @@
 module.exports = {
-  siteMetadata: {
-    title: `Jamie Liu`,
-    description: `Jamie's personal website.`,
-    author: `@jamieliu386`,
-  },
-  plugins: [
-    `gatsby-plugin-material-ui`,
-    `gatsby-plugin-react-helmet`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
-    },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      },
-    },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
-  ],
-}
+	siteMetadata: {
+		title: 'Jamie Liu',
+		description: 'Jamie’s personal website.',
+		author: '@jamieliu386'
+	},
+	plugins: [
+		'gatsby-plugin-material-ui',
+		'gatsby-plugin-react-helmet',
+		{
+			resolve: 'gatsby-source-filesystem',
+			options: {
+				name: 'images',
+				path: `${__dirname}/src/images`
+			}
+		},
+		'gatsby-transformer-sharp',
+		'gatsby-plugin-sharp',
+		{
+			resolve: 'gatsby-plugin-manifest',
+			/* eslint-disable camelcase */
+			options: {
+				name: 'Jamie Liu',
+				short_name: 'JL',
+				start_url: '/',
+				background_color: '#fff',
+				theme_color: '#000',
+				display: 'minimal-ui',
+				icon: 'src/images/gatsby-icon.png' // This path is relative to the root of the site.
+			}
+			/* eslint-enable camelcase */
+		}
+		// this (optional) plugin enables Progressive Web App + Offline functionality
+		// To learn more, visit: https://gatsby.dev/offline
+		// 'gatsby-plugin-offline',
+	]
+};

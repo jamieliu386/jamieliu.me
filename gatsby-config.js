067c4a01370302ec0b200ea07c106ad2f70143d3
Jamie Liu <jamie.liu386@gmail.com>
2020-06-26 18:37:46 -0400

    Add gatsby transformer json

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,6 +14,13 @@ module.exports = {
 				path: `${__dirname}/src/images`
 			}
 		},
+		'gatsby-transformer-json',
+		{
+			resolve: 'gatsby-source-filesystem',
+			options: {
+				path: `${__dirname}/src/data/`
+			}
+		},
 		'gatsby-transformer-sharp',
 		'gatsby-plugin-sharp',
 		{

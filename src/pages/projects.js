b75872f8570e7112b29d80ebbee6f97af505cf42
Jamie Liu <jamie.liu386@gmail.com>
2020-06-25 22:21:36 -0400

    Add other page skeletons

---
+++ src/pages/projects.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import Layout from '../components/layout';
+import SEO from '../components/seo';
+
+const ProjectPage = () =>
+	<Layout selected='projects'>
+		<SEO title='Projects' />
+	</Layout>;
+
+export default ProjectPage;
---
+++ src/pages/teaching.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import Layout from '../components/layout';
+import SEO from '../components/seo';
+
+const TeachingPage = () =>
+	<Layout selected='teaching'>
+		<SEO title='Teaching' />
+	</Layout>;
+
+export default TeachingPage;

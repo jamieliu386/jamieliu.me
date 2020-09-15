b7354d19d4fe1883488ef29c51589423a9eaf99d
Jamie Liu <jamie.liu386@gmail.com>
2020-09-15 15:42:22 -0400

    Make project cards clickable

--- src/components/ProjectPage/ProjectCard/ProjectCard.js
+++ src/components/ProjectPage/ProjectCard/ProjectCard.js
@@ -2,10 +2,11 @@ import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
-import Paper from '@material-ui/core/Paper';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardContent from '@material-ui/core/CardContent';
 import GitHubIcon from '@material-ui/icons/GitHub';
 import LinkIcon from '@material-ui/icons/Link';
-import MoreIcon from '@material-ui/icons/MoreHoriz';
 import SVGImg from '../../SVGImg';
 import useMediaQuery from '@material-ui/core/useMediaQuery';
 import ProjectInfo from '../ProjectInfo/ProjectInfo';
@@ -26,38 +27,36 @@ export default function ProjectCard(props) {
 	const isSmall = useMediaQuery('(max-width:500px)');
 
 	return (
-		<Paper className={styles.card}>
-			<Box
-				display='flex'
-				flexDirection={isSmall ? 'column' : 'row'}
-				justifyContent='space-between'
-				alignItems={isSmall ? 'flex-start' : 'center'}
-			>
-				<Typography variant='body1' style={{ fontSize: '1.5rem', fontWeight: 600 }}>
-					{title}
-				</Typography>
-				<Box>
-					<a href={github} className={styles.icon} target='_blank'
-						rel='noopener noreferrer'>
-						<GitHubIcon titleAccess='code'/>
-					</a>
-					<a href={link} className={styles.icon} target='_blank'
-						rel='noopener noreferrer'>
-						<LinkIcon titleAccess='project'/>
-					</a>
-					<span onClick={handleClickOpen} style={{ cursor: 'pointer' }}>
-						<MoreIcon />
-					</span>
-				</Box>
-			</Box>
-			<Typography variant='body1' className={styles.tech}>{tech}</Typography>
-			<SVGImg src={img} width={dim.width} height={dim.height} />
-			<Typography variant='body1' className={styles.desc}>{desc}</Typography>
-			<ProjectInfo {...props}
-				handleClose={handleClose}
-				open={open}
-			/>
-		</Paper>
+		<Card variant='outlined' className={styles.card}>
+			<CardActionArea onClick={handleClickOpen}>
+				<CardContent>
+					<Box
+						display='flex'
+						flexDirection={isSmall ? 'column' : 'row'}
+						justifyContent='space-between'
+						alignItems={isSmall ? 'flex-start' : 'center'}
+					>
+						<Typography variant='body1' style={{ fontSize: '1.5rem', fontWeight: 600 }}>
+							{title}
+						</Typography>
+						<Box>
+							<a href={github} className={styles.icon} target='_blank'
+								rel='noopener noreferrer'>
+								<GitHubIcon titleAccess='code'/>
+							</a>
+							<a href={link} className={styles.icon} target='_blank'
+								rel='noopener noreferrer'>
+								<LinkIcon titleAccess='project'/>
+							</a>
+						</Box>
+					</Box>
+					<Typography variant='body1' className={styles.tech}>{tech}</Typography>
+					<SVGImg src={img} width={dim.width} height={dim.height} />
+					<Typography variant='body1' className={styles.desc}>{desc}</Typography>
+				</CardContent>
+			</CardActionArea>
+			<ProjectInfo {...props} handleClose={handleClose} open={open} />
+		</Card>
 	);
 }
 
